47c8f609d3f60ead0c03b2f91c41ccb67b57132f
xing.heng <xing.heng@tom.com>
2023-07-30 15:21:20 +0800

    fix: react 18 Warning: ReactDOM.render is no longer supported in React 18.

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "@/assets/styles/common.less";
 import style from "./index.less";
 import HelloBigImage from "@/assets/images/helloworld-big-image.jpg";
 import smallImage from "@/assets/images/small-image.png";
+import { sum } from "./utils";
 
-const App = () => (<div className={style.appContainer}>
-  Hello World!
-  <img style={{ width: '50%'}} src={HelloBigImage} alt="" />
-  <img src={smallImage} alt="" />
-</div>);
+const App = () => {
+  const a = sum(1, 2);
+  console.log(a);
+  return (<div className={style.appContainer}>
+    Hello World!
+    <img style={{ width: '50%'}} src={HelloBigImage} alt="" />
+    <img src={smallImage} alt="" />
+  </div>);
+}
 
-ReactDOM.render(
+//  ReactDOM.render is no longer supported in React 18.
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
